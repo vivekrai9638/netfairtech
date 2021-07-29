dc07945e9badf1d77c6d10a4579e142e93e48b73
vivekrai9638 <kumarvivek8290@gmail.com>
2021-07-29 13:06:53 +0530

    jwt

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 const auth = async function (req, res, next) {
   try {
     const token = req.header("Authorization").replace("Bearer ", "");
-    const decoded = await jwt.verify(token, "thisissignature");
+    const decoded = await jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findOne({
       _id: decoded._id,
       "tokens.token": token,
